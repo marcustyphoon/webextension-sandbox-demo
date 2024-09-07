f1f4b92a1cb676996f7e2c3cf503d6a8b4f0602b
Marcus <marcustyphoon@gmail.com>
2024-09-07 12:25:31 -0700

    prettier

--- src/main_world/index.js
+++ src/main_world/index.js
@@ -8,38 +8,42 @@
   const controller = new AbortController();
   window.removeExtensionDemoListener = () => controller.abort();
 
-  document.documentElement.addEventListener('extension-demo-injection-request', async (event) => {
-    const { detail, target } = event;
-    const { id, path, args } = JSON.parse(detail);
+  document.documentElement.addEventListener(
+    'extension-demo-injection-request',
+    async (event) => {
+      const { detail, target } = event;
+      const { id, path, args } = JSON.parse(detail);
 
-    try {
-      moduleCache[path] ??= await import(path);
-      const func = moduleCache[path].default;
+      try {
+        moduleCache[path] ??= await import(path);
+        const func = moduleCache[path].default;
 
-      if (target.isConnected === false) return;
+        if (target.isConnected === false) return;
 
-      const result = await func.apply(target, args);
-      target.dispatchEvent(
-        new CustomEvent('extension-demo-injection-response', {
-          detail: JSON.stringify({ id, result }),
-        }),
-      );
-    } catch (exception) {
-      target.dispatchEvent(
-        new CustomEvent('extension-demo-injection-response', {
-          detail: JSON.stringify({
-            id,
-            exception: {
-              message: exception.message,
-              name: exception.name,
-              stack: exception.stack,
-              ...exception,
-            },
+        const result = await func.apply(target, args);
+        target.dispatchEvent(
+          new CustomEvent('extension-demo-injection-response', {
+            detail: JSON.stringify({ id, result }),
           }),
-        }),
-      );
-    }
-  }, { signal: controller.signal });
+        );
+      } catch (exception) {
+        target.dispatchEvent(
+          new CustomEvent('extension-demo-injection-response', {
+            detail: JSON.stringify({
+              id,
+              exception: {
+                message: exception.message,
+                name: exception.name,
+                stack: exception.stack,
+                ...exception,
+              },
+            }),
+          }),
+        );
+      }
+    },
+    { signal: controller.signal },
+  );
 
   document.documentElement.dispatchEvent(new CustomEvent('extension-demo-injection-ready'));
 }
